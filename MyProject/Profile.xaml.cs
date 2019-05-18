36b211c9bbac289941ce6da0590a22857748d87b
Nastya <sakun_nastya@mail.ru>
2019-05-19 00:14:44 +0300

    FirstCommit

---
+++ MyProject/Profile.xaml.cs
@@ -0,0 +1,67 @@
+﻿using MyProject.database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MyProject
+{
+    /// <summary>
+    /// Логика взаимодействия для Profile.xaml
+    /// </summary>
+    public partial class Profile : UserControl
+    {
+        public Profile()
+        {
+            InitializeComponent();
+        }
+
+        private void Delete_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBoxResult result = MessageBox.Show("Вы точно хотите удалить аккаунт?", 
+                "Удаление",MessageBoxButton.YesNo, MessageBoxImage.None,MessageBoxResult.No);
+           
+            if (result==MessageBoxResult.Yes)
+            {
+                UserRepository userR = new UserRepository();
+                userR.Delete(userR.Get(mail.Text));
+                name.Text = "пусто";
+                mail.Text = "пусто";
+
+                Вход_Регистрация sign = new Вход_Регистрация();
+                sign.Show();
+
+                MainWindow window = (MainWindow)Application.Current.MainWindow;
+                window.Start.Children.Clear();
+            }
+        }
+
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+            name.Text = "пусто";
+            mail.Text = "пусто";
+
+            Вход_Регистрация sign = new Вход_Регистрация();
+            sign.Show();
+
+            MainWindow window = (MainWindow)Application.Current.MainWindow;
+            window.Start.Children.Clear();
+        }
+
+        private void Redact_Click(object sender, RoutedEventArgs e)
+        {
+            ProfileRedact redact = new ProfileRedact();
+            redact.Show();
+        }
+    }
+}
---
+++ MyProject/Вход_Регистрация.xaml.cs
@@ -0,0 +1,66 @@
+﻿using MyProject.database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MyProject
+{
+    /// <summary>
+    /// Логика взаимодействия для Вход_Регистрация.xaml
+    /// </summary>
+    public partial class Вход_Регистрация : Window
+    {
+        public Вход_Регистрация()
+        {
+            InitializeComponent();
+            
+        }
+
+        UserRepository userR = new UserRepository();
+
+        private void Input_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow w = (MainWindow)Application.Current.MainWindow;
+            Начало_обучения n = new Начало_обучения();
+            n.Block.Children.Clear();
+            Profile p = new Profile();
+            n.AnotherPages.Children.Add(p);
+            w.Start.Children.Add(n);
+
+            var thisUser = userR.Get(mail1.Text);
+            User user = new User();
+            p.name.Text =thisUser.name;
+            p.mail.Text = mail1.Text;
+            p.topics.Text += thisUser.t_count;
+            this.Close();
+        }
+
+        private void Reg_Click(object sender, RoutedEventArgs e)
+        {            
+            User user = new User { Id = mail2.Text, name = name2.Text, password = pass2.Password };
+            userR.Create(user);
+            MessageBox.Show("Регистрация выполнена успешно!");            
+
+            MainWindow w = (MainWindow)Application.Current.MainWindow;
+            Начало_обучения n = new Начало_обучения();
+            n.Block.Children.Clear();
+            Profile p = new Profile();
+            n.AnotherPages.Children.Add(p);
+            w.Start.Children.Add(n);
+            p.name.Text= name2.Text;
+            p.mail.Text= mail2.Text;
+
+            this.Close();
+        }
+    }
+}
